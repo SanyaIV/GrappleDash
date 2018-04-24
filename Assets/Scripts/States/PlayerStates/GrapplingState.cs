31e90123067daea826c8394ef64b3663791dbd70
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-24 15:28:37 +0200

    Fix infinigrapple f

--- Assets/Scripts/States/PlayerStates/GrapplingState.cs
+++ Assets/Scripts/States/PlayerStates/GrapplingState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ad70bb7eb848dc5aa4fcbabcf7e209ac13ab04ffe0843f131c2cc91ce206f66
-size 1211
+oid sha256:0dc83190d73972f7e00667134de6a6d1420a6e51312083185fdfff66997b314e
+size 1361
