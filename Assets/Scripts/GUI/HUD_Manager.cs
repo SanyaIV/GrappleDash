907356d92a37c8d3dcbd0333ddfa8f9f94221fa3
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-24 12:39:21 +0200

    Renamed _currentDashes to CurrentDashes yup

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af74361c1007e1ff537bcb151964632bb755b6ddd61dfa1ccde963bdf1baf3a
-size 1037
+oid sha256:9bb50223ee7346f17308f99bcf002e19c359f9086bc0cc42b285ad3faecd3491
+size 1036
