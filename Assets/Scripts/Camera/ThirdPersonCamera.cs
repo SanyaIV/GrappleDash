f0ccd97e0481f6889e8c60f31cd771d43c6858fb
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-24 13:17:08 +0200

    Fixed Camera Offset + Camera Collision Yup

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a58df1156b6ef3d191b124679a87a51f9273d3b6f899459b8ee227d891d0547
-size 3483
+oid sha256:e2badad583ca319dfcb661e0339f8ea9f04e21804e19f7bb43cb9a36aedcced8
+size 3205
