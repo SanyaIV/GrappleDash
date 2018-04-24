42e6cec165327a9ca00fca065177e9b06fe2655c
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-24 11:16:45 +0200

    GUI 4.4.5 Fixed empty dash

--- Assets/Scripts/HUD_Manager.cs
+++ Assets/Scripts/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f265bf37c676d2a42aa8536b9282d25bfe85eff5ce5e1d784a82c186344f308c
+oid sha256:a31dc45f84dbbd5989b1c4764032e88ef757886a190923e73784d5a20bcc1d86
 size 710
