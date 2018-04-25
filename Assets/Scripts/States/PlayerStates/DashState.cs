84afb7de28dcd45012ae8db48a3dabe42f0bf98e
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-25 09:27:37 +0200

    A dash of dash and a shake of shake Nyaa~

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e80c29eee50df0a2533529c2c1ae815dd5d7c5d4c73b7f48abd860ce774c8a1
-size 2010
+oid sha256:d4391928478a5fe41cbe2ed249a110305cb53c9ae08141a5053c69dc1265e357
+size 2161
