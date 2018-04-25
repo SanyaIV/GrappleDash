fb2c1541e5f7abcd07626f62ff6d856598d8a4f1
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-25 14:48:45 +0200

    Removed Dash reset cooldown It caused issues, i.e. dash dash dash, jump right as you touch the ground, should reset dash but doesn't because cooldown.

--- Assets/Scripts/States/PlayerStates/GroundState.cs
+++ Assets/Scripts/States/PlayerStates/GroundState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90a8ec6cad4d9ed1b9a995b83a959844ba0d3ac19a2a60ef9efbe19375534ad8
-size 3626
+oid sha256:bbc948172618c19896f7e56c1c078b6a65bb4370c6d9530420bdef32e22535d4
+size 3664
