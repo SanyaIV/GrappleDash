06d70738b08e9a0c199027b8f8e0b8bd30a1d8ed
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-26 10:07:29 +0200

    Gradual grapple speed increase

--- Assets/Scripts/States/PlayerStates/GrapplingState.cs
+++ Assets/Scripts/States/PlayerStates/GrapplingState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32eb8a989e5a6dd4cd571e531d4e19a500b024860ef302dd3aebf7b51ff14450
-size 2080
+oid sha256:aae991a8ead9f883cf353179a5c7156a3e23f7fa1cba2aea8d706814d070c567
+size 2491
