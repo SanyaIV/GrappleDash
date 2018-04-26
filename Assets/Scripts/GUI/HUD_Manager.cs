b5ed5801b5ca3ab69fbd4eb9be6ddc5765ab77d4
EkuDeku <ekuman.k@gmail.com>
2018-04-26 11:54:15 +0200

    Some GUI, Eku and Boss stuff, and application of a value in Grapple state so it could work for me

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bb50223ee7346f17308f99bcf002e19c359f9086bc0cc42b285ad3faecd3491
-size 1036
+oid sha256:3a9b86dcc7df9f8c0dfecac7bd5eaaa588f3400dbc5273c3be665ee648b59c78
+size 1046
