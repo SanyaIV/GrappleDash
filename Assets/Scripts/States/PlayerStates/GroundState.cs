788ad42c9d1e2c097688c2df7618fb89c788b327
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-27 15:11:05 +0200

    groundstate

--- Assets/Scripts/States/PlayerStates/GroundState.cs
+++ Assets/Scripts/States/PlayerStates/GroundState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc948172618c19896f7e56c1c078b6a65bb4370c6d9530420bdef32e22535d4
-size 3664
+oid sha256:b61a3ec87d1282cb342f7dbe953606295afd3e294ddfca8c6b22934dcd94d504
+size 3674
