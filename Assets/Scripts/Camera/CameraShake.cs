d41c20fb875662fa690379a37519655cf3fe8b48
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-30 15:50:58 +0200

    CameraShake stuff

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aefa271aac7bd5a950aec9e73e858440d484dff6f50916e8cadcaaa9ebc7d73
-size 953
+oid sha256:ce64122c2ba582b20526012057a0fdb5d5c8dc1eb8a926d409880cf4768f9ac1
+size 1051
--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4972cb6f09fb66c95a48b3ee80e3806839b62af5b78530341bcbbdbb50cc3b6e
+oid sha256:478994aad76db6f08e6699b4c143b92173d4bc98f2b6fa6e77328a344ed4b945
 size 2186
