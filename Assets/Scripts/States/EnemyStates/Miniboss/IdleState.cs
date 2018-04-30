33c58c82c3e8c12f178943f216ea0c988b5e6b5c
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-30 17:20:53 +0200

    Miniboss now does Chaotic Defense

--- Assets/Scripts/States/EnemyStates/Miniboss/IdleState.cs
+++ Assets/Scripts/States/EnemyStates/Miniboss/IdleState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67ea3371cd903bf7568748e07e21cff376e2fdced1155e0d5420c1ed3db69fb7
-size 618
+oid sha256:661106d1d0f8a47303ef2b24f8bf10569c777a7b986cd4991d8cfcddf3058c76
+size 677
--- Assets/Scripts/States/EnemyStates/Miniboss/PhaseOne.cs
+++ Assets/Scripts/States/EnemyStates/Miniboss/PhaseOne.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6918c54d48e57c6c502fa934a3f043f49f648abd02358c8310f9008e677b895
-size 604
+oid sha256:fa4fa095ab96b84c6b8b6e2a5d4ba1dcf098a50d4a39eb1d5a70dcdfb5d5e3d8
+size 1750
