4b9734a375adb823d69a10736f35d1c95f333a5a
SanyaIV <sanyaivlitvyak@gmail.com>
2018-04-30 15:07:19 +0200

    Some tweaks

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89ed034184ce5c34806457092eaf799a7fd0888c7bb4979a08868205c93f705c
-size 3346
+oid sha256:2e680f0c99d8927b4ed138198fcafc415508d97a45b4abc1be9f6bd54bba60ba
+size 3307
