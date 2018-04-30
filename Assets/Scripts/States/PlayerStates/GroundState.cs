1170a77d244ef2b522482ecfa68610e64110b715
Erik Larsson <erla5582@win.dsv.su.se>
2018-04-30 09:43:12 +0200

    Fixed som ground and plaform things.

--- Assets/Scripts/States/PlayerStates/GroundState.cs
+++ Assets/Scripts/States/PlayerStates/GroundState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5172b7efa4c645662c0f5d9df66b694871bc62bbd1c3f20504d898771734e5e0
-size 4114
+oid sha256:ab1a31ae93c8800c067798b0fb32ce9ce4192341964832e7648940120ac1bbed
+size 4236
