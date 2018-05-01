0c74d4e91c84d780c13dc1fbcdc31ac19c06e666
LxuisCyphxR <fquinterosureta@gmail.com>
2018-05-01 23:44:12 +0200

    Enemy02 patrol

---
+++ Assets/Scripts/Enemies/Enemy_02/EnemyAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c5875023ce0a783661403aaf8fabc8f660c395e00234340874bcfbc22958a1ec
+size 343
---
+++ Assets/Scripts/Enemies/Enemy_02/NPCBaseFinalStateMachine.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ff54358194285a4a798c2d19c56d633403ac5472a5f837d14a84711dea435f0
+size 525
