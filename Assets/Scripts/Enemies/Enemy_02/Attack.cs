82a1702458e9eb18bedfe79c0c74abec3ed15fdf
LxuisCyphxR <fquinterosureta@gmail.com>
2018-05-02 00:30:46 +0200

    Enemy 02 Attack

---
+++ Assets/Scripts/Enemies/Enemy_02/Attack.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86bc7d146c68b4d5b29d343158ce52d720f09042424804c71855223dcae042f9
+size 984
--- Assets/Scripts/Enemies/Enemy_02/EnemyAI.cs
+++ Assets/Scripts/Enemies/Enemy_02/EnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08aa9ee59da26259d7fbb755e18074b329a044b917b25611b4000f20d574247c
-size 509
+oid sha256:8aa2fd856bfb55774db9baac9836a03cf1ec10bf5e3a396f12a552b15463c6bd
+size 946
