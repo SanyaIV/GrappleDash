61e838710639b7e5051fab6d8658a362f6f99162
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-01 12:29:24 +0200

    Grapple in dash

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:137608520c5d6ca71f3f890a0f9176e43446e6e9494c52afdc115430d8ab2989
-size 2150
+oid sha256:23dab2bcb2eeb626e5b53dbcaa5edb1c62bb4cfc6907d6bdab46967586f5f3f6
+size 2308
