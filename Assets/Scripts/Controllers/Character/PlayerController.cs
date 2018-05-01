e2e9c9a73c3e6c926075c905f6c5f8ddb7075425
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-01 13:35:11 +0200

    Add force to RigidBody in collisions with Dash.

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99d9e3fb838642f2583887520d0c0a82a3257a3fc52eff4f63308f7320e946ff
-size 3205
+oid sha256:a9ebca653d95a7f9f8b1ffa26531b30b6abc27ebaa0bb609da1d12b8035d06a7
+size 3691
