29ecaae294486f8bbf0e4ec2fa4db53719c96690
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-01 14:56:34 +0200

    Some more fixes

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31d365b7a812e504eadef76fa61ab9900c971fbea42bea17006daf5d85721268
-size 3892
+oid sha256:5b1aeb6950553e555172514c43afbfc39559d10bfffd4a34376fca591be6f1a6
+size 3753
