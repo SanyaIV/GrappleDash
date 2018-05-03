b4cb7b0cbcfeafb0501ee44abaa87675c47711e2
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-03 11:49:55 +0200

    Miniboss death stuff

--- Assets/Scripts/States/EnemyStates/Miniboss/MBDeadState.cs
+++ Assets/Scripts/States/EnemyStates/Miniboss/MBDeadState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdabdf6a01d02d5cf439f29a427897cceca4a392f7fc9a84297afa745d60278a
-size 709
+oid sha256:c98c7df636ddddd9b8af7245f0791d68967f8b4b4324402906f48044fc87ce85
+size 942
