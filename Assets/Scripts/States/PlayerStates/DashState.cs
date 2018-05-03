aa3f6452acf9d8cf3bb7ce96f793187f66071466
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-03 12:49:29 +0200

    GhostDash

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23223c9e82820eae09b8a93fba3827045525410577b01aee44075d5833bc3937
-size 2629
+oid sha256:76d844ccb660d59eed08c9cf52e71b3cc196244da52afcb8c05d26ebf1d2ebc0
+size 2897
