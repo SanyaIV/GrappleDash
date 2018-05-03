6f78317d305f263a8729aa7c21593fcf6492ef65
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-03 16:33:27 +0200

    Merge fix

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba6cc50cbdec78bad663c854baf796b69503c7707cd571048b87b323e89cfa0
-size 2928
+oid sha256:76d844ccb660d59eed08c9cf52e71b3cc196244da52afcb8c05d26ebf1d2ebc0
+size 2897
