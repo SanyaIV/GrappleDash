df252a62e97bfc95908301d296bd09b1bccd5508
EkuDeku <ekuman.k@gmail.com>
2018-05-03 11:45:05 +0200

    UI and DAHS

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c599fd06626cab46d590efdbcc4db66832a17a0b27a568ae423359651010b705
-size 2755
+oid sha256:7c5b6d40a363ebd04032cebdee3f05483f77bd420c6df6022d1cab293093981d
+size 2965
