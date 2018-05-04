d6660669524afd6cb7b9de8fafd9b733712c6fcb
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-04 16:33:50 +0200

    Boss & touch damage stuff

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d1132b39ac09b5a1f9c4df19b6d1b22e6194d7b31e55d3c35ca9aee7cdcde87
-size 4232
+oid sha256:98c9962e7f07bad28fb24fb6af69995e5991740262a812068bb2c9f5d37c1662
+size 4473
