70832ee1dccf51552410e2228ade23b55a9d4f42
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-06 14:29:44 +0200

    Boss stuff

--- Assets/Scripts/Events/BossScript.cs
+++ Assets/Scripts/Events/BossScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fa49391535e2c8612c69f1ca30170d7de9f7223db13ada881374ae7a7793022
-size 1707
+oid sha256:484c04a75870db18176aaf275848d2a0c18f975aa6f28a254b173d3b434afd20
+size 1799
