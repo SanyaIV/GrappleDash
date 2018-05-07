6090f55c42e5b875ec694626dbd3b20e53f30d5a
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 16:23:12 +0200

    sda

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35cc9982cf588e63d5a139b40ddc06d9f6e2620358b79fd0b256a353c1a85548
-size 5066
+oid sha256:e85d31406c1dd0488bcbe3479bbb041469453b6d970dadb7aa8fe553693c44c4
+size 5095
