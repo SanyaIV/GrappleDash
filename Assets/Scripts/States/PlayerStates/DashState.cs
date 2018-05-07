120fe77b22fbc8ea41d0c36eab9cfe05c0ca3450
Jonny <Nichgild@gmail.com>
2018-05-07 15:06:40 +0200

    m m

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:388816c05c81b950350fef9b398e52b6c3b57b767bd0d1e732807db1bf1bd4f0
-size 2964
+oid sha256:e872978ece4b599eb36ec6b39e08b86d4702504d088cce115e6860b9ec6b0d9d
+size 3288
