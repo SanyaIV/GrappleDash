2978ead886a2c6f639b829405202868fc407ac86
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 13:51:59 +0200

    asd

--- Assets/Scripts/States/PlayerStates/ShieldState.cs
+++ Assets/Scripts/States/PlayerStates/ShieldState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ae434ef35a55d513363fb008fc97df5940efce90403558fe04b50b507bd1fdc
-size 3203
+oid sha256:c2640f706e0386c76a415f12ab2a06cb9447ab0c266209da29f5e0b111f8de61
+size 3323
