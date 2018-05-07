78ccc871c5710a98359ac48c1bb511fbf2e7b595
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 20:16:02 +0200

    asd

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:460f80f7bd8a981ab2c978166242c8d10da267451e5602ae118204c4ba5ca45e
-size 3427
+oid sha256:3b7bd74b84dbe21ffb6643ff24f2294c294c4efe2225b55a75d3a4146a342c50
+size 3493
