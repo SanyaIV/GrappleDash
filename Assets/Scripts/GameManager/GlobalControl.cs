4150782193605dd2e995dd6ec935a8d36898e5d0
erik <isakssonocherik@hotmail.se>
2018-05-07 15:06:31 +0200

    Game manager restartar från hub eller nuvarande ninvå

--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6155c563ee8041b42aa6645aa092c58fdda78f1e3fd993c39fd917d8a4d4d412
-size 473
+oid sha256:016a875af410fd376af68c9c826d354abe2e1cd95bd818a61249acc16f909172
+size 1015
