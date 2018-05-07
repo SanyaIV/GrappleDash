eeac3668787d216f6063d38351f58e52bc3ba946
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 21:10:05 +0200

    asd

--- Assets/Scripts/Characters/Enemies/Enemy_02/Projectile.cs
+++ Assets/Scripts/Characters/Enemies/Enemy_02/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9decff708ee80ce64fa779f1816675dc8dce9ba74a14513df28e7d7c3a18e2dd
-size 1477
+oid sha256:d07387be4df6e0d38ee2eb35a076bbf2c1f2dcdecb8988135cf681633e8ea3ba
+size 1511
