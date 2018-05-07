6cdc083559c98047eff7a62139a8dee4afb8c4ee
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 09:58:21 +0200

    asd

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a127f38b280c40db9efc25bf7d41498abb9994035ee8250974dbb136c5dee367
-size 3844
+oid sha256:599d95365c5116e2206a92c1fb6fa2d4ab8ce5d81cf7dbcd7ba33bb8cf1a9efe
+size 4091
---
+++ Assets/Scripts/States/PlayerStates/VictoryState.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cc17df4a0cffae23771f4944657eedaa6eb2da4d340ed15e1fe9476170f3055
+size 798
