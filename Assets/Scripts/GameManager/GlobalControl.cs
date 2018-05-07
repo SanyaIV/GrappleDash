55cbd608847165d0fc816d0288a7985d34a4b139
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 21:33:12 +0200

    removed debug

--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:367a55a2d8e79fa4e89cf2507c384fcf15157de5c71fcc5d7438933aa6519121
-size 1746
+oid sha256:1d3f6f680d65ef1b5b05cea8dc5b075823ac35cf5eca425a14a35305c0895aa0
+size 1711
