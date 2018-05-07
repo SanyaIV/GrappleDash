646b725cba8531a8c8d71a9bd8497aadf91a72d6
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-07 22:31:30 +0200

    Killbox and damage correctly

--- Assets/Scripts/Characters/Health.cs
+++ Assets/Scripts/Characters/Health.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6090b3e64e342da67516e9315b597493c798d7cb9fa7c5e3667a98b653eeb79b
-size 3249
+oid sha256:b61a1d6489d95ac54590c3cbd29af7fc2d4bfdcb05840d3217075ccd26b22dbe
+size 3639
--- Assets/Scripts/Props/KillBox/KillerBox.cs
+++ Assets/Scripts/Props/KillBox/KillerBox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd88cb5ebc2c1e3748e2955d0c9645ad932d405faae1311317c419f021b43d8d
-size 515
+oid sha256:d55ddb32415c1dd52fd811c87b9308835e97c2e898a8e8a0344bc0427d269d75
+size 794
