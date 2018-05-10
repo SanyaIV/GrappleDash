639ce556b85b1a67d16a869c9147d8f173f5af53
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-10 08:26:43 +0200

    Ändrade grappleljudet så att det slutar när spelaren släpper grapple.

--- Assets/Scripts/States/PlayerStates/GrapplingState.cs
+++ Assets/Scripts/States/PlayerStates/GrapplingState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d418009243c3ccaaaad36e9773f4d2ab6730e9662987418cd2430057ab3fcf94
-size 4069
+oid sha256:3bfe417b353f6eb7e7856483b9a44255ebe63f71bca90334a25c6819b7ab9f09
+size 4186
