922c25aea331b5fcfffff1ccabf45f9ed57b053f
Jonny <Nichgild@gmail.com>
2018-05-11 11:56:37 +0200

    .

--- Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOneChargeState.cs
+++ Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOneChargeState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85d8a465c5dcda42d05aedb17fe2702b2d4e67f975bcca1d85ef012f815e31d1
-size 2238
+oid sha256:627548f987b3e6cd97df9afda33b8e7ec60d41f9d3ba3761a3c74889619041a1
+size 1944
