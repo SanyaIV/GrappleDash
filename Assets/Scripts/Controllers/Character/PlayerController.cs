ed701920263c33b7dd19b16cd377604dfb875835
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-11 16:46:46 +0200

    Different pushback, hopefully works a bit better.

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f06d1354c281c6a24fa76b8c5fa541c4a6866001b5159bf25e7a8e134482ef2e
-size 5441
+oid sha256:9556cca4845c81bcead92eec92276128dfe8cce570cd5059c47daf3b47b7a074
+size 5592
--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7103044c714b540a4299993e7002b607a0b2d59409a1ee9b97dee7eb8f4326e
-size 4045
+oid sha256:9e4f4ec61067bd422ba8331f21dfe1b8b8acdca38bc564d2069214be750089d6
+size 4349
