a18c6b2f1b231955a84c490125cf86fb31227e7d
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-11 10:15:13 +0200

    removed debug from a script

--- Assets/Scripts/Events/First Fight/CallPlattform.cs
+++ Assets/Scripts/Events/First Fight/CallPlattform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50b109cbfeb9a1f50df066e2469d4a936892b152e4591639c3dfa2419ccc72ff
-size 268
+oid sha256:90167de9864fdf681a3bb05d3972c2ee5fc9eec8201e16afc4ea63b1607bc20f
+size 230
