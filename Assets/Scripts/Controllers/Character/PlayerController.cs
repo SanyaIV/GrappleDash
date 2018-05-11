129f282ca855f2aafc559fc14a25e66f167319c2
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-11 16:48:31 +0200

    Removed Debuggs

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9556cca4845c81bcead92eec92276128dfe8cce570cd5059c47daf3b47b7a074
-size 5592
+oid sha256:9ce8e3613e16b7a0eb6781f23f32d14fc9ff701ac45697bb6c4755cdd5baa62c
+size 5551
