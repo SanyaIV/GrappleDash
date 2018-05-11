4a1e78575e1cf0f783209d809b1259ded5f91444
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-11 08:39:03 +0200

    First enemy needs to die

---
+++ Assets/Scripts/Events/First Fight/CallPlattform.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50b109cbfeb9a1f50df066e2469d4a936892b152e4591639c3dfa2419ccc72ff
+size 268
--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49e22b43b41be1f5b376612ab9467b1e2f95e317664d03c89bbcbb40d28265e4
-size 2783
+oid sha256:d6bec2362d0ddf6177f360094e0d5f5e910f17a9ced864f54517479c139299b9
+size 2789
