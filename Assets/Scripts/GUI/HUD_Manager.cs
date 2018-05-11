8507a6a08b86b0d255a43a2cd32e3605ba0d6f7f
EkuDeku <ekuman.k@gmail.com>
2018-05-11 17:48:37 +0200

    Some stuff

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1d4032bc56fc780b1b56b9915c121391fc7131047f4ec1ba639c5897efc8d53
-size 4380
+oid sha256:9398d2bdabc11c1682534b5369482b88fda4390aab8a6dc2a8af2a00db536fc1
+size 4620
