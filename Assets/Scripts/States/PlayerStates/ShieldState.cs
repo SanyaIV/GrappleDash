6b6ae9e58f1ae732f88871102379b820f7b908da
SanyaIV <SanyaIvLitvyak@gmail.com>
2018-05-14 15:33:12 +0200

    Added shield recharge on enemy collision

--- Assets/Scripts/States/PlayerStates/ShieldState.cs
+++ Assets/Scripts/States/PlayerStates/ShieldState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48a1ddbad3678488706db68dfa208f4562937e514fc57e38a65c81cdb4f18b42
-size 3439
+oid sha256:5d1db65813fd7817be3b5c7f3d298da6853e58cb854436d59a746e03483b8348
+size 3678
