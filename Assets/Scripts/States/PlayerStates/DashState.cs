25cf130c972f63b7df3afd2d30203f52aaf6e81b
Christian Ekman <chek3604@win.dsv.su.se>
2018-05-14 10:42:59 +0200

    Fixed weird infinidash bug.

--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,155 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9e4f4ec61067bd422ba8331f21dfe1b8b8acdca38bc564d2069214be750089d6
-size 4349
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player/States/Dash")]
+public class DashState : PlayerState {
+
+    private bool _dashSuccess = false;
+    private bool _bounce = false;
+
+    [Header("Dash Settings")]
+    public const int DEFAULT_DASH_AMOUNT = 1;
+    public const int MAX_DASH_AMOUNT = 3;
+    public int CurrentMaxDashAmount;
+    public int CurrentDashes;
+    public float GroundDashCooldown;
+    private float _lastDash;
+    private bool _ghostDash;
+
+    [Header("Dash Travel")]
+    public float DashSpeed = 5f;
+    public float DashExitSpeed;
+    public float MaxDashTime = 0.2f;
+    private float _dashTimer = 0f;
+    private Vector3 _direction;
+
+    [Header("Sounds")]
+    private AudioSource source;
+    public AudioClip dash;
+
+    public override void Initialize(Controller owner)
+    {
+        base.Initialize(owner);
+
+        if (GlobalControl.Instance != null)
+            CurrentMaxDashAmount = GlobalControl.CurrentDashes;
+        else
+            CurrentMaxDashAmount = 3;
+
+        ResetDashes();
+        source = _controller.GetComponent<AudioSource>();
+        source.clip = dash;
+    }
+
+
+    public override void Enter()
+    {
+        _controller._collision = CollisionFlags.None;
+        _direction = _controller.TPCamera.forward;
+        _ghostDash = false;
+        _bounce = false;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetButtonDown("Grapple") && _controller.GetState<GrapplingState>().CheckGrappable())
+        {
+            _controller.TransitionTo<GrapplingState>();
+            return;
+        }
+        if (Input.GetButtonDown("Dash"))
+            _ghostDash = true;
+
+        if (!UpdateDash()) return;
+
+        _dashTimer += Time.deltaTime;
+
+        _controller._collision = _charCtrl.Move(_controller.MoveDir * Time.deltaTime);
+    }
+
+    public override void Exit()
+    {
+        _dashTimer = 0f;
+        if (_dashSuccess)
+        {
+            _dashSuccess = false;
+            CurrentDashes--;
+            _lastDash = Time.time;
+
+            if(!_bounce)
+                _controller.MoveDir = _controller.MoveDir.normalized * DashExitSpeed;
+
+            if ((_controller._collision & CollisionFlags.Above) != 0 || (_controller._collision & CollisionFlags.Sides) != 0)
+                CameraShake.SetIntensity(0.5f);
+        }
+        source.Stop();
+    }
+
+    private bool UpdateDash()
+    {
+        if (!_dashSuccess && CurrentDashes > 0 && (Time.time - _lastDash >= GroundDashCooldown || !_controller.IsGrounded()))
+        {
+            _dashSuccess = true;
+            _controller.MoveDir = _direction * DashSpeed;
+            PlayDashSound();
+        }
+        else if ((_controller._collision & CollisionFlags.Below) != 0 && _controller.IsGrounded() && _controller.PreviousState is AirState)
+        {
+            _controller.TransitionTo<GroundState>();
+            return false;
+        }
+        else if (_ghostDash && CurrentDashes >= 0)
+        {
+            _controller.TransitionTo<DashState>();
+            return false;
+        }
+        else if (!_dashSuccess || _dashTimer >= MaxDashTime || (Time.time - _lastDash <= GroundDashCooldown && _controller.IsGrounded()))
+        {
+            StopDash();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void StopDash(bool bounce = false)
+    {
+        if (_controller.CurrentState is DashState)
+        {
+            _bounce = bounce;
+
+            if (_controller.IsGrounded())
+                _controller.TransitionTo<GroundState>();
+            else
+                _controller.TransitionTo<AirState>();
+        }
+    }
+
+    public void ResetDashes()
+    {
+        CurrentDashes = CurrentMaxDashAmount;
+    }
+
+    public void DashUp()
+    {
+        _controller.HUD.StartCoroutine(_controller.HUD.DashUpgraded());
+        CurrentDashes = ++CurrentMaxDashAmount;
+        GlobalControl.AddDash();
+    }
+
+    public void PlayDashSound() {
+         if (!source.isPlaying)
+            source.PlayOneShot(dash, 0.3f);
+    }
+    
+    public Vector3 GetDirection()
+    {
+        if (_dashSuccess)
+            return _direction;
+        else
+            return Vector3.zero;
+    }
+}
--- Assets/Scripts/States/PlayerStates/GroundState.cs
+++ Assets/Scripts/States/PlayerStates/GroundState.cs
@@ -1,3 +1,152 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ab0e416bcdc0bb0da9bee548e2342ed982ed0f07c40d8c3bc8188c8053736b02
-size 4538
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player/States/Ground")]
+public class GroundState : PlayerState {
+
+    [Header("Movement")]
+    public float Acceleration = 100f;
+    public float ExtraFriction = 30f;
+    public float ExtraSecondaryFriction = 30f;
+    public float StopSlidingLimit = 1.5f;
+    [HideInInspector] public bool InputGotten = false;
+
+    [Header("Ground")]
+    public float StickToGroundForce = 10f;
+
+    [Header("Jumping")]
+    public MinMaxFloat JumpHeight;
+    public float TimeToJumpApex = 0.5f;
+    public float InitialJumpDistance = 0.15f;
+    public float MaxGhostAirJumpTime = 0.2f;
+    [HideInInspector] public MinMaxFloat JumpVelocity;
+    [HideInInspector] public float TimeOfAirJump;
+
+    [Header("Temporary Variables")]
+    private RaycastHit hit;
+
+    private float Friction { get { return Acceleration / _controller.MaxSpeed; } }
+
+    public override void Initialize(Controller owner)
+    {
+        base.Initialize(owner);
+
+        _controller.Gravity = (2 * JumpHeight.Max) / Mathf.Pow(TimeToJumpApex, 2);
+        JumpVelocity.Max = _controller.Gravity * TimeToJumpApex;
+        JumpVelocity.Min = Mathf.Sqrt(2 * _controller.Gravity * JumpHeight.Min);
+        TimeOfAirJump = -100f;
+    }
+
+    public override void Enter()
+    {
+        InputGotten = false;
+
+        //Reset amount of dashes
+        if (_controller.PreviousState is AirState && Time.time - TimeOfAirJump <= MaxGhostAirJumpTime)
+            UpdateJump();
+
+        _controller.GetState<DashState>().ResetDashes();
+    }
+
+    public override void Exit()
+    {
+        transform.parent = null;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetButtonDown("Jump"))
+            UpdateJump();
+
+        if (Input.GetButtonDown("Dash"))
+            _controller.TransitionTo<DashState>();
+
+        if (Input.GetButtonDown("Grapple"))
+            _controller.TransitionTo<GrapplingState>();
+
+        /*hit = _controller.GroundCheck();
+
+        UpdateMovement();
+        UpdateFriction();
+
+        if (hit.collider != null)
+            _controller.MoveDir.y = -StickToGroundForce;
+        else
+            _controller.TransitionTo<AirState>();
+
+        StopSliding();*/
+
+        _controller._collision = _charCtrl.Move(MoveDir * Time.deltaTime);
+
+        /*if (hit.collider != null)
+            _controller.MoveDir.y = 0f;*/
+    }
+
+    public override void FixedUpdate()
+    {
+        hit = _controller.GroundCheck();
+
+        UpdateMovement();
+        UpdateFriction();
+
+        if (hit.collider != null)
+            _controller.MoveDir.y = -StickToGroundForce;
+        else
+            _controller.TransitionTo<AirState>();
+
+        StopSliding();
+    }
+
+    private void UpdateMovement()
+    {
+        Vector3 input = _controller.Input;
+        input = transform.forward * input.z + transform.right * input.x;
+
+        if (hit.collider != null && hit.collider.CompareTag("Platform"))
+        {
+            hit.collider.GetComponent<Platform>().Activate();
+            transform.parent = hit.collider.gameObject.transform.parent;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        input = Vector3.ProjectOnPlane(input, hit.normal).normalized;
+
+        MoveDir += input * Acceleration;
+
+        if(input.magnitude > 0)
+        {
+            InputGotten = true;
+
+            if (MoveDir.magnitude > _controller.MaxSpeed)
+                MoveDir = MoveDir.normalized * _controller.MaxSpeed;
+        }
+    }
+
+    private void UpdateFriction()
+    {
+        float extraFriction = InputGotten ? _controller.Input.magnitude < _controller.InputRequiredToMove ? ExtraFriction : 0.0f : ExtraSecondaryFriction; //_controller.Input.magnitude < _controller.InputRequiredToMove ? ExtraFriction : 0.0f;
+        float friction = Mathf.Clamp01((Friction + extraFriction) * Time.fixedDeltaTime);
+        MoveDir -= MoveDir * friction;
+    }
+
+    private void StopSliding()
+    {
+        if (MoveDir.magnitude < StopSlidingLimit && _controller.Input.magnitude < _controller.InputRequiredToMove)
+            MoveDir = Vector3.zero;
+    }
+
+    public void UpdateJump()
+    {
+        transform.position += Vector3.up * InitialJumpDistance;
+        _controller.MoveDir.y = JumpVelocity.Max;
+        _controller.GetState<AirState>().GhostJumpAllowed = false;
+        _controller.GetState<AirState>().CanCancelJump = true;
+        if(!(_controller.CurrentState is AirState))_controller.TransitionTo<AirState>();
+    }
+}
