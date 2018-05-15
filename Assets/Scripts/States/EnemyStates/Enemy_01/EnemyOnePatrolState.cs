c6823e06785aa2ad59e20a10a707620d041b28ed
erik <isakssonocherik@hotmail.se>
2018-05-15 12:52:20 +0200

    mmm

--- Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOnePatrolState.cs
+++ Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOnePatrolState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d193e9568deec3d5e5471c7ffd453d85c5db2aff3b1ae9aef38f7ff95f101732
-size 2754
+oid sha256:fd8617c207a82380afad9475c4d19e5a760af2ada64f849215682d97113b0fe0
+size 3092
