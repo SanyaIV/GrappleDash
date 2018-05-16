7f4ef0403258c9a960a6ebc400d7eae558024eaa
SanyaIV <SanyaIVLitvyak@gmail.com>
2018-05-16 11:45:25 +0200

    cave stuff

--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a232993c6c4574815925ff644e2a8c8841cb1840d07e664be7a6990e3c2d6d6
-size 1427
+oid sha256:4b3af7a0a6a40dc3a76e36386819b06ec78e393b577603abdfc791742fb31568
+size 1246
