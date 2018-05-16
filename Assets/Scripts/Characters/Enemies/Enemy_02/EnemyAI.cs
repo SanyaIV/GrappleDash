27bc32996b87e5807ad50415e53f266ca902a7ab
SanyaIV <SanyaIVLitvyak@gmail.com>
2018-05-16 15:14:49 +0200

    projectile fuckery

--- Assets/Scripts/Characters/Enemies/Enemy_02/EnemyAI.cs
+++ Assets/Scripts/Characters/Enemies/Enemy_02/EnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfe33dff296f30abaca87a1718eea01c3b5bb703db2930ebc812917efd534530
-size 1422
+oid sha256:5310995647bd67b804473cd4850efed2dd04ec6999103c8d2b1ef2c39c5d1968
+size 1527
