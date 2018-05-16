38acf8e407b8ff9f0e07713024c3b6a967a858dc
SanyaIV <SanyaIVLitvyak@gmail.com>
2018-05-16 12:26:20 +0200

    Jump sounds

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb1b9c038f10b3decc69a0c08036bcfc6e54f946cd87f4edda282ffd5bbf882c
-size 5905
+oid sha256:8bc53371d8413f98b736bd466787fe6f7c4beb52abc92fec69359a04eda9f0c2
+size 6039
--- Assets/Scripts/States/PlayerStates/DashState.cs
+++ Assets/Scripts/States/PlayerStates/DashState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af07dfdc4326daa37e35dcdb91c08e95d5e758ae466cd7210d971767268741ca
-size 4377
+oid sha256:8ceecb184902bb55befebe6236fb865c687ade71d23675b16307fc68cbaa920b
+size 4387
