6e9196810ba0d1c98b82e50557c28e788a7f93f6
SanyaIV <SanyaIVLitvyak@gmail.com>
2018-05-16 12:44:09 +0200

    fixed pickup -> hub issue

--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cca81b195f2a8a9a60ec97f7acab6ddc1b8943c94023796dd710fd11ef528d7
-size 3437
+oid sha256:875f0dde9e0315e69091cd39cb02ca2aa7167a66854a2b2d8c90cfb47f56f974
+size 3612
