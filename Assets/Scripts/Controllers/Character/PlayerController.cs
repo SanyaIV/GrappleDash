a7c2d77f4ec51b1d3b932afd57c66668aad9a12b
SanyaIV <SanyaIVLitvyak@gmail.com>
2018-05-16 11:24:31 +0200

    Some fixes to the dark and stuff

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf48f4b0baf3c18972985451b6300f3138aba03770e94a3c7830f1c946b3de1
-size 5850
+oid sha256:cb1b9c038f10b3decc69a0c08036bcfc6e54f946cd87f4edda282ffd5bbf882c
+size 5905
