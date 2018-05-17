4d5fd74a8df2a9cc4b8618c044443d35d59b146b
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-17 14:37:41 +0200

    fixed

--- Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOnePatrolState.cs
+++ Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOnePatrolState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85d49304a0dc18c8e8f16adac428039637add865807efc81039065c84be0719b
-size 3255
+oid sha256:57211a351a090e27aa5ea80a70f92580f7df6cddc7de36024933549fdf9a5351
+size 3336
