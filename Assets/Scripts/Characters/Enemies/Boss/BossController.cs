375cc8c3b83eccc0732382aedd3bc6218397a591
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-18 13:52:40 +0200

    anims

--- Assets/Scripts/Characters/Enemies/Boss/BossController.cs
+++ Assets/Scripts/Characters/Enemies/Boss/BossController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ab9974b379dcfd7a3ebd1d20edfe2e1b6db826aa51b9e310d8f424fd9922873
-size 856
+oid sha256:03a03317d78df37872bfe3357d8739089f2ad6ccb96e3e10d69aee8b2963d7f9
+size 1053
