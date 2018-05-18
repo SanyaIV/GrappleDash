b8a7c5e2aba63e6b6a26d7bfb67d5e6aa0d21f9f
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-18 15:31:23 +0200

    sadf

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1126d6b9e4746b19fd174d1f46c2348d5fa792e7ade84e0853115364995decd7
-size 5509
+oid sha256:ef31646f6c75d1097bfa7b8a6e48f85015e695fbaf52640d81c179296be33102
+size 5514
