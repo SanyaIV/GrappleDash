3a2fee4fa5c2ca4e84cd4a944845bbadfd848266
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-21 10:14:15 +0200

    Awesome death

--- Assets/Scripts/GUI/HUD_Manager.cs
+++ Assets/Scripts/GUI/HUD_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef31646f6c75d1097bfa7b8a6e48f85015e695fbaf52640d81c179296be33102
-size 5514
+oid sha256:8d3238a3c94b207959ded1f576f56ab421fbb1742aa34fac8f61fa079231448b
+size 5516
--- Assets/Scripts/States/PlayerStates/DeadState.cs
+++ Assets/Scripts/States/PlayerStates/DeadState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5307cf60e6cc9b5664a5cfff3247e3d846d43d5a05f4ca7a202e8aadd429989b
-size 1430
+oid sha256:6c1de5a2d872d73f28ae477c12d7ea35f3e24d37eb7fe6e69831f832ef7f4648
+size 1475
