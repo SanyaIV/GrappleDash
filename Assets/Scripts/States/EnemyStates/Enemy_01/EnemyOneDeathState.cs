238cfa65419d2663790a44bccd0e000dd885d5b9
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-21 13:11:21 +0200

    add enemy death sounds

--- Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOneDeathState.cs
+++ Assets/Scripts/States/EnemyStates/Enemy_01/EnemyOneDeathState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90eb744e298aed7d492fc4cf4b7f2b7bbf6e848d3599b62460821cb4fe7e2523
-size 1165
+oid sha256:088aa8fb90901e3805148589160185d81c611772ee46d696831f58d8c2ea0dcc
+size 971
