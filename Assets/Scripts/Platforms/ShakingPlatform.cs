5524c6c3e257cc37e1a45f4dbbe4cde1adbc2e64
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-21 08:28:20 +0200

    Fixat ljudproblem med fallande plattformar

--- Assets/Scripts/Platforms/ShakingPlatform.cs
+++ Assets/Scripts/Platforms/ShakingPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0abc77e4ecaa21891248ca5e9174699dac33c02090487ddcdec0023962f66d04
-size 1881
+oid sha256:a3640807c448102be3170b5d5bbb9fb889d94b97966c667cd995e93e6a1d1e57
+size 2097
