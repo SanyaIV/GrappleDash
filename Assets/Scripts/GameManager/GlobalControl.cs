fd6e7ac46d132527fe9e4d3de2c1ab013ee264be
erik <isakssonocherik@hotmail.se>
2018-05-22 14:26:07 +0200

    fix

--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e6a1380b4b26c3e6c25ef4e41140f7a9ad9b5709b4387ab276c4a2462d07a67
-size 5047
+oid sha256:b4a9d2e6a13ab567a1e85e0f4447a238be01e63210b40ad8eea5df70e7aa5527
+size 4745
