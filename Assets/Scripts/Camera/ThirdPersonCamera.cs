9ba10165276938b0ce3bbda1b39be815a83e0bc7
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-23 09:03:36 +0200

    Think I fixed portals

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:411dd71e38c37aec88225a50c5ce60ae0425ade7e65a0a6d897a0c768ee5059d
-size 3523
+oid sha256:bcb5a57242b0a6b9a8d712d8956943584e7f84b0522f9a4e02202d46aaf8657b
+size 3572
