13a3e754cec14af47348313d4396c805a844fafb
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-23 13:00:19 +0200

    Fix GUI and stuffs

--- Assets/Scripts/Controllers/Character/PlayerController.cs
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b109cbc375744b64dcfac6945c46c3ddce608fe6dbd441d994e7f22a693ea299
-size 6187
+oid sha256:061956a3c1551c1786c4575c97b70341dbeb2389280c8b3e307f9df8b3039b45
+size 6220
