eb077989f84a985c51c097205b9eb65a92a3ea06
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-23 09:13:31 +0200

    fix portal and pause timer in hub

--- Assets/Scripts/GameManager/GlobalControl.cs
+++ Assets/Scripts/GameManager/GlobalControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4a9d2e6a13ab567a1e85e0f4447a238be01e63210b40ad8eea5df70e7aa5527
-size 4745
+oid sha256:9345e5fa99af6aeb1d1ce27b631349831f99b4c5be0bebeffa0f2649ed3c39f3
+size 4941
