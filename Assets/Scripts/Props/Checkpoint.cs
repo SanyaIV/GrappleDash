a5e4e197fdba23558f806825e3efeb1dd23dd968
SanyaIV <sanyaivlitvyak@gmail.com>
2018-05-23 11:09:45 +0200

    checkpoints

--- Assets/Scripts/Props/Checkpoint.cs
+++ Assets/Scripts/Props/Checkpoint.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7e14c080311a5ed2b78c7aae745f133010da3e9d3f466058c339790c6da1ed
-size 972
+oid sha256:93559fd57ab143d7920a2cbfed8de72c659a2864dc0302e6fe85b42d18d90d7c
+size 1377
